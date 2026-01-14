974f1930c58acc2c402826ad9b3554e6ba9fa03d
Aidan Saunders <baconman434@gmail.com>
2026-01-14 18:04:00 -0500

    save and load scroll

--- ScrollSaver/save.js
+++ ScrollSaver/save.js
@@ -11,6 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log("hello");
         
         const url = await getCurrentTabUrl();
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+              chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                func: sendScrollPosition
+              })
+              .then(requestScrollPosition());
+            })
         
         chrome.storage.local.set({'mySavedUrl': url}, function() {
             console.log('URL saved!');
@@ -22,5 +30,22 @@ document.addEventListener('DOMContentLoaded', function() {
 async function getCurrentTabUrl() {
     const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
     console.log("Current URL: ", tab.url);
-    return tab.url
+    return tab.url;
+}
+
+async function requestScrollPosition() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const response = await chrome.tabs.sendMessage(tab.id, {message: "scrollXY"});
+    chrome.storage.local.set({'scrollPos': response.scrollPos}, function() {
+            console.log('scroll saved');
+        });
+}
+
+function sendScrollPosition(){
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.message == "scrollXY") {
+            sendResponse({ scrollPos: [window.scrollX,window.scrollY] });
+        }
+    }
+);
 }
--- ScrollSaver/script.js
+++ ScrollSaver/script.js
@@ -1,5 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('loadPage').addEventListener('click', function() {
-    chrome.tabs.create({ url: "http://www.crouton.net/" });
-  });
+document.getElementById('loadPage').addEventListener('click', function() {
+  chrome.storage.local.get(['mySavedUrl', 'scrollPos'], async function(result) {
+        if (result.mySavedUrl) {
+            const url = await getCurrentTabUrl();
+
+            if(url!=result.mySavedUrl){
+              chrome.tabs.create({ url: result.mySavedUrl });
+            }
+
+            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+              chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                func: loadScrollPos
+              })
+              .then(() => chrome.tabs.sendMessage(tabs[0].id,{scrollPos:[result.scrollPos[0],result.scrollPos[1]]}));
+            })
+            
+        } else {
+            console.log("no url");
+        }
+    });
 });
+
+function loadScrollPos() {
+    chrome.runtime.onMessage.addListener(message=>{
+      if (message.scrollPos) {
+          window.scrollTo(message.scrollPos[0], message.scrollPos[1]);
+      }
+  });
+}
+
