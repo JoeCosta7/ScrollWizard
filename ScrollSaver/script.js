1b331ce4291861d2b94b178de5867522d08a6154
fishpyramid <hihihican@gmail.com>
2026-05-29 04:14:36 -0400

    fixed an oopsie

--- ScrollSaver/script.js
+++ ScrollSaver/script.js
@@ -15,66 +15,6 @@ async function handleButtonClick(savedUrl, savedPosition) {
   }
 };
 
-function handleDeleteClick(savedUrl, savedPosition) {
-        chrome.storage.local.get(['saves'], function(result) {
-        const entries = result.saves || [];
-        const urlEntryIndex = entries.findIndex(entry => entry.url === savedUrl);
-        const index = entries[urlEntryIndex].positions.findIndex(pos => 
-            pos[0] === savedPosition[0] && pos[1] === savedPosition[1]
-        );
-
-        entries[urlEntryIndex].positions.splice(index, 1);
-        if (entries[urlEntryIndex].positions.length === 0) {
-          entries.splice(urlEntryIndex, 1);
-        }
-
-        chrome.storage.local.set({ 'saves' : entries}, function() {
-            console.log('URL and scroll position deleted');
-            displayUrls(entries);
-        });
-
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.scripting.executeScript( {
-            target: { tabId: tabs[0].id },
-            func: (x, y) => {
-              document.getElementById(`anchor-${x}-${y}`).remove();
-            },
-            args: [savedPosition[0],savedPosition[1]]
-            });
-        })
-    });
-}
-
-function handleDeleteClickFromFull(savedUrl, savedPosition) {
-        chrome.storage.local.get(['saves'], function(result) {
-        const entries = result.saves || [];
-        const urlEntryIndex = entries.findIndex(entry => entry.url === savedUrl);
-        const index = entries[urlEntryIndex].positions.findIndex(pos => 
-            pos[0] === savedPosition[0] && pos[1] === savedPosition[1]
-        );
-
-        entries[urlEntryIndex].positions.splice(index, 1);
-        if (entries[urlEntryIndex].positions.length === 0) {
-          entries.splice(urlEntryIndex, 1);
-        }
-
-        chrome.storage.local.set({ 'saves' : entries}, function() {
-            console.log('URL and scroll position deleted');
-            DisplayUrlsFull(entries);
-        });
-
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.scripting.executeScript( {
-            target: { tabId: tabs[0].id },
-            func: (x, y) => {
-              document.getElementById(`anchor-${x}-${y}`).remove();
-            },
-            args: [savedPosition[0],savedPosition[1]]
-            });
-        })
-    });
-}
-
 function loadScrollPos(x, y) {
   window.scrollTo({
     left: x, 
